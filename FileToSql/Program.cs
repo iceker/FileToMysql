7cca6db3d10fa68043313d16a34c4e97e2d9310a
iceker <iceker@live.com>
2017-12-19 23:00:07 +0800

    init

--- FileToSql/Program.cs
+++ FileToSql/Program.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +14,29 @@ namespace FileToSql
     {
         static void Main(string[] args)
         {
+            string[] lines = File.ReadAllLines(@"D:\DATA\mogujie.txt");
+            foreach (var item in lines)
+            {
+                using (MySqlConnection conn = new MySqlConnection("server=localhost;user id=root;password=root;database=cloudsoft_global"))
+                {
+                    DataSet ds = new DataSet();
+                    var first = item.Split(',')[0];
+                    var sql = "select * from mogujie where name='" + first+"'";
+                    MySqlDataAdapter adapter = new MySqlDataAdapter(sql, conn);
+                    try
+                    {
+                        adapter.Fill(ds);
+                        
+                    }
+                    catch (Exception)
+                    {
+
+                        throw;
+                    }
+                    
+                }
+            }
+
         }
     }
 }
