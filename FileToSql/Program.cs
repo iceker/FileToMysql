264a81175551e91870331636c74687d7b40267d3
iceker <iceker@live.com>
2017-12-21 08:45:35 +0800

    complet

--- FileToSql/Program.cs
+++ FileToSql/Program.cs
@@ -17,24 +17,53 @@ namespace FileToSql
             string[] lines = File.ReadAllLines(@"D:\DATA\mogujie.txt");
             foreach (var item in lines)
             {
+                DataSet ds = new DataSet();
+                DataSet ds1 = new DataSet();
+
                 using (MySqlConnection conn = new MySqlConnection("server=localhost;user id=root;password=root;database=cloudsoft_global"))
                 {
-                    DataSet ds = new DataSet();
-                    var first = item.Split(',')[0];
-                    var sql = "select * from mogujie where name='" + first+"'";
-                    MySqlDataAdapter adapter = new MySqlDataAdapter(sql, conn);
-                    try
+                    var line = item.Split(',');
+                    for (int j = 0; j < line.Count(); j++)
                     {
-                        adapter.Fill(ds);
-                        
-                    }
-                    catch (Exception)
-                    {
+                        var sql = "select * from mogujie where name='" + line[j] + "'";
+                        ds = MySqlHelper.ExecuteDataset(conn, sql);
+                        for (int i = 0; i < ds.Tables[0].Rows.Count; i++)
+                        {
 
-                        throw;
+                            var sql2 = "select * from mogujie_clean where fcid = '" + ds.Tables[0].Rows[i][4] + "'";
+                            int rows = MySqlHelper.ExecuteDataset(conn, sql2).Tables[0].Rows.Count;
+                            if (rows < 1)
+                            {
+                                conn.Close();
+                                conn.Open();
+                                var sql3 = "insert into mogujie_clean(createdDate,name,parentName,fcid,rawData) values(@createdDate,@name,@parentName,@fcid,@rawData)";
+                                int executerows = MySqlHelper.ExecuteNonQuery(conn, sql3,
+                                    new MySqlParameter("@createdDate", ds.Tables[0].Rows[i][1]),
+                                    new MySqlParameter("@name", ds.Tables[0].Rows[i][2]),
+                                    new MySqlParameter("@parentName", ds.Tables[0].Rows[i][3]),
+                                    new MySqlParameter("@fcid", ds.Tables[0].Rows[i][4]),
+                                    new MySqlParameter("@rawData", ds.Tables[0].Rows[i][5])
+                                //new MySqlParameter("@createdDate", MySqlDbType.DateTime) { Value=ds.Tables[0].Rows[i][1]},
+                                //new MySqlParameter("@name", MySqlDbType.VarChar) { Value = item },
+                                //new MySqlParameter("@parentName", MySqlDbType.VarChar) { Value = ds.Tables[0].Rows[i][3] },
+                                //new MySqlParameter("@fcid", MySqlDbType.VarChar) { Value = ds.Tables[0].Rows[i][4] },
+                                //new MySqlParameter("@rawData", MySqlDbType.VarChar) { Value = ds.Tables[0].Rows[i][5] }
+                                );
+                                if (executerows > 0)
+                                {
+                                    Console.WriteLine("插入成功");
+                                }
+                                else
+                                {
+                                    Console.WriteLine("插入失败");
+                                }
+
+                            }
+
+                        }
                     }
-                    
                 }
+                
             }
 
         }
